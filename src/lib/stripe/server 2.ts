53083f6f545f1c4fe94ab3cf2ab7fce8173e8991
Kate Gajewski <kategajewski@gmail.com>
2026-04-30 00:11:57 -0400

    Fix Vercel build and clean duplicate files

--- src/lib/stripe/server 2.ts
+++
@@ -1,8 +0,0 @@
-import Stripe from "stripe";
-import { env } from "@/lib/env";
-
-export const stripe = env.stripeSecretKey
-  ? new Stripe(env.stripeSecretKey, {
-      apiVersion: "2026-02-25.clover",
-    })
-  : null;
